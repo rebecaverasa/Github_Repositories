3f3c930eee6dee193f8a9993d3d7541fec07fc44
rebecaverasa <rebecaverasa@gmail.com>
2024-04-09 01:30:58 -0300

    Pacoted depreciados removidos

--- src/Components/RepositoriesTable/RepositoriesTable.jsx
+++ src/Components/RepositoriesTable/RepositoriesTable.jsx
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleModal } from "../../redux/actions";
